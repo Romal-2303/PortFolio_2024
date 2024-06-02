566249971fc3e9dd2f392384982089d53d550fbc
Romal-2303 <romalg13@gmail.com>
2024-06-02 15:25:54 +0530

    My first commit

---
+++ src/assets/index.js
@@ -0,0 +1,101 @@
+import bwmap from "./backgrounds/bw-map.jpeg";
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/white-abstract.png";
+import worldmap from "./backgrounds/world-map.png";
+
+import logo from "./logo/logo-black.png";
+import logotext from "./logo/logo-text-black.png";
+
+import Stoic_Image from "./personal/Stoic_Image.png";
+
+import backend from "./icons/backend.png";
+import ux from "./icons/ux.png";
+import frontend from "./icons/frontend.png";
+import prototyping from "./icons/prototyping.png";
+import github from "./icons/github.png";
+import LinkedIn from "./icons/linkedIn.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import scss from "./tech/scss.png";
+import typescript from "./tech/typescript.png";
+import graphql from "./tech/graphql.png";
+import postgresql from "./tech/postgresql.png";
+import rubyrails from "./tech/rubyrails.png";
+
+import coverhunt from "./company/coverhunt.png";
+import factriai from "./company/factriai.png";
+import suseco from "./company/suseco.png";
+import microverse from "./company/microverse.png";
+
+import AdminDashboard from "./projects/admin_dashboard.png";
+import TicTacToe from "./projects/game_tictactoe.png";
+import GeoGuesser from "./projects/game_geoguesser.png";
+import BarChart from "./projects/3d_barchart.png";
+import nyeusi from "./projects/nyeusi.png";
+import space from "./projects/space-hub.png";
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  Stoic_Image,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  LinkedIn,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  rubyrails,
+  reactjs,
+  redux,
+  scss,
+  typescript,
+  AdminDashboard,
+  TicTacToe,
+  GeoGuesser,
+  BarChart,
+  nyeusi,
+  space,
+  coverhunt,
+  factriai,
+  suseco,
+  microverse,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,236 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  scss,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  AdminDashboard,
+  TicTacToe,
+  GeoGuesser,
+  BarChart,
+  nyeusi,
+  space,
+  coverhunt,
+  factriai,
+  suseco,
+  microverse,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: frontend,
+  },
+  // {
+  //   title: 'Backend Developer',
+  //   icon: backend,
+  // },
+  // {
+  //   title: 'UI/UX Design',
+  //   icon: ux,
+  // },
+  {
+    title: "System Design",
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Scss",
+    icon: scss,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  // {
+  //   name: "Rails",
+  //   icon: rubyrails,
+  // },
+  // {
+  //   name: "graphql",
+  //   icon: graphql,
+  // },
+  // {
+  //   name: "postgresql",
+  //   icon: postgresql,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Frontend Developer",
+    company_name: "Sustainability Economics",
+    icon: suseco,
+    iconBg: "#333333",
+    date: "Nov 2023 - Present",
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Facti Ai",
+    icon: factriai,
+    iconBg: "#333333",
+    date: "July 2022 - Nov 2023",
+  },
+];
+
+const projects = [
+  {
+    id: "project-1",
+    name: "Admin Dashboard",
+    description:
+      "An intuitive React-based admin dashboard featuring dynamic charts and interactive maps to streamline business analytics and decision making",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: AdminDashboard,
+    repo: "https://github.com/Romal-2303/Admin_Dashboard",
+    demo: "https://glowing-trifle-847fd2.netlify.app/",
+  },
+  {
+    id: "project-2",
+    name: "Tic Tac Toe",
+    description:
+      "A Tic Tac Toe game built in react, offering a smooth gameplay and an engaging user experience.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: TicTacToe,
+    repo: "https://github.com/Romal-2303/Tic-Tac-Toe",
+    demo: "https://wonderful-florentine-ac4e62.netlify.app/",
+  },
+  {
+    id: "project-3",
+    name: "Geo-Guesser",
+    description:
+      "A guessing game built in react offering a rich quiz experience.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: GeoGuesser,
+    repo: "https://github.com/Romal-2303/GeoGuesser",
+    demo: "https://reliable-biscuit-290d49.netlify.app/",
+  },
+  {
+    id: "project-4",
+    name: "3D Barchart",
+    description: `Third Dimensional bar chart built with three js.`,
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: BarChart,
+    repo: "https://github.com/Romal-2303/3d_barchart",
+    demo: "https://genuine-pegasus-764cec.netlify.app/",
+  },
+];
+
+export { services, technologies, experiences, projects };
